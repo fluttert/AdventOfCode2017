54c3f74753f344ef8599c27a667458f079282785
Ernst Fluttert <e.fluttert@gmail.com>
2017-12-03 23:03:53 +0100

    Solution Day 3 part 1
    without generating the spiral

---
+++ AdventOfCode2017/Challenges/Day03.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode2017.Interfaces;
+using System;
+
+namespace AdventOfCode2017.Challenges
+{
+    public class Day03 : IChallengeDay
+    {
+        /// <summary>
+        /// Manhattan distance to the center of a spiral-number-sequence
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string Part01(string input)
+        {
+            int candidate = int.Parse(input);
+            int manhattanDistance = 0;
+            if (candidate == 1) { return manhattanDistance.ToString(); }
+
+            // determine ring
+            int steps = 0;
+            int ring = 1;
+            while ((ring * ring) < candidate)
+            {
+                steps++;
+                ring += 2;
+            }
+            int[] minimumsteps = new int[] {
+                (ring * ring) - 1* steps,
+                (ring * ring) - 3* steps,
+                (ring * ring) - 5 *steps ,
+                (ring * ring) - 7* steps
+            };
+            bool found = false;
+            for (int i = 0; i <= steps; i++)
+            {
+                foreach (int minstep in minimumsteps)
+                {
+                    if (minstep + i == candidate || minstep - i == candidate)
+                    {
+                        manhattanDistance = steps + i;
+                        found = true;
+                        break;
+                    }
+                }
+            }
+            return manhattanDistance.ToString();
+        }
+
+        public string Part02(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string input = "368078";
+    }
+}
---
+++ AdventOfCode2017Tests/Day03Test.cs
@@ -0,0 +1,28 @@
+﻿using AdventOfCode2017.Challenges;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace AdventOfCode2017Tests
+{
+    [TestClass]
+    public class Day03Test
+    {
+        [TestMethod]
+        public void Day03Part01()
+        {
+            var day03 = new Day03();
+            day03.Part01("1").Should().Be("0");
+            day03.Part01("2").Should().Be("1");
+            day03.Part01("9").Should().Be("2");
+            day03.Part01("25").Should().Be("4");
+            day03.Part01("12").Should().Be("3");
+            day03.Part01("23").Should().Be("2");
+            day03.Part01("36").Should().Be("5");
+            day03.Part01("37").Should().Be("6");
+            day03.Part01("26").Should().Be("5");
+            day03.Part01("43").Should().Be("6");
+            day03.Part01("38").Should().Be("5");
+            day03.Part01("1024").Should().Be("31");
+        }
+    }
+}
