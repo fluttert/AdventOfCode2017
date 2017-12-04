911bb6480661dad44bfe764b16e3ffe3315b3783
Ernst Fluttert <e.fluttert@gmail.com>
2017-12-04 20:37:19 +0100

    Day04 solutions part1 and part2

--- AdventOfCode2017/Program.cs
+++ AdventOfCode2017/Program.cs
@@ -7,7 +7,7 @@ namespace AdventOfCode2017
     {
         private static void Main(string[] args)
         {
-            var day = new Day03();
+            var day = new Day04();
 
             //Console.WriteLine(day.Part01(day.input));
             Console.WriteLine(day.Part02(day.input));
---
+++ AdventOfCode2017Tests/Day04Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AdventOfCode2017.Challenges;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2017Tests
+{
+    [TestClass]
+    public class Day04Test
+    {
+        [TestMethod]
+        public void Day04Part01() {
+            var day = new Day04();
+            day.Part01("aa bb cc dd ee").Should().Be("1");
+            day.Part01("aa bb cc dd aa").Should().Be("0");
+            day.Part01("aa bb cc dd aaa").Should().Be("1");
+            day.Part01("aa aa").Should().Be("0");
+            day.Part01(@"aa aa
+bb bb
+aa bb").Should().Be("1");
+
+
+        }
+
+        [TestMethod]
+        public void Day04Part02()
+        {
+            var day = new Day04();
+            day.Part02("aa bb cc dd ee").Should().Be("1");
+            day.Part02("abcde xyz ecdab").Should().Be("0");
+
+
+
+        }
+    }
+}
