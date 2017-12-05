6c4cd05301fb8ba81827e31f279422dac41051df
Ernst Fluttert <e.fluttert@gmail.com>
2017-12-05 19:50:14 +0100

    Day 05, solved first part

---
+++ AdventOfCode2017Tests/Day05Test.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode2017.Challenges;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2017Tests
+{
+    [TestClass]
+    public class Day05Test
+    {
+        [TestMethod]
+        public void Day05Part01()
+        {
+            var day = new Day05();
+            day.Part01(@"0
+3
+0
+1
+-3").Should().Be("5");
+        }
+    }
+}
