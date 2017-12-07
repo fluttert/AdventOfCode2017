b119fa0323da94170623b61d2d6204b1a65001de
Ernst Fluttert <e.fluttert@gmail.com>
2017-12-07 22:35:05 +0100

    Day 07 part 1

---
+++ AdventOfCode2017Tests/Day07Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode2017.Challenges;
+
+namespace AdventOfCode2017Tests
+{
+    [TestClass]
+    public class Day07Test
+    {
+        [TestMethod]
+        public void Day07Part01() {
+            var day = new Day07();
+            var input = @"pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)";
+            day.Part01(input).Should().Be("tknk");
+        }
+    }
+}
