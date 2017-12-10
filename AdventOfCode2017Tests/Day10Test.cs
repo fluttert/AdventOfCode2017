438daf94e40761942dbd6639cabc3d3a234f3b0d
Ernst Fluttert <e.fluttert@gmail.com>
2017-12-10 17:11:25 +0100

    Day  10, part 2

--- AdventOfCode2017Tests/Day10Test.cs
+++ AdventOfCode2017Tests/Day10Test.cs
@@ -14,5 +14,18 @@ namespace AdventOfCode2017Tests
             day.ListLength = 5;
             day.Part01("3, 4, 1, 5").Should().Be("12");
         }
+
+        [TestMethod]
+        public void Day10Part02()
+        {
+            var day = new Day10();
+            day.Part02("").Should().Be("a2582a3a0e66e6e86e3812dcb672a272");
+
+            day.Part02("1,2,3").Should().Be("3efbe78a8d82f29979031a4aa0b16a9d");
+            
+            day.Part02("AoC 2017").Should().Be("33efeb34ea91902bb2f59c9920caa6cd");
+            
+            day.Part02("1,2,4").Should().Be("63960835bcdc130f0b66d7ff4f6a5a8e");
+        }
     }
 }
