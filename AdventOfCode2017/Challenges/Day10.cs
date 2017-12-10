9040a38c5d61960955b576570916727724314ae7
Ernst Fluttert <e.fluttert@gmail.com>
2017-12-10 17:21:13 +0100

    Day 10, part 2

--- AdventOfCode2017/Challenges/Day10.cs
+++ AdventOfCode2017/Challenges/Day10.cs
@@ -1,5 +1,7 @@
 ﻿using AdventOfCode2017.Interfaces;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2017.Challenges
 {
@@ -14,7 +16,6 @@ namespace AdventOfCode2017.Challenges
             var list = new int[ListLength];
             for (int i = 0; i < ListLength; i++) { list[i] = i; }
 
-
             int currentPosition = 0;
             int skipSize = 0;
             var sequences = Array.ConvertAll(input.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries), int.Parse);
@@ -23,7 +24,8 @@ namespace AdventOfCode2017.Challenges
                 // select elements in list to be reversed
                 int halfSeq = seq / 2; // 6/2=3, 5/2=2
                 int seqenceEndIdex = currentPosition + seq - 1;
-                for (int i = 0; i < halfSeq; i++) {
+                for (int i = 0; i < halfSeq; i++)
+                {
                     int startIndex = (currentPosition + i) % ListLength;
                     int endIndex = (seqenceEndIdex - i) % ListLength;
 
@@ -31,7 +33,6 @@ namespace AdventOfCode2017.Challenges
                     int swap = list[startIndex];
                     list[startIndex] = list[endIndex];
                     list[endIndex] = swap;
-
                 }
 
                 // move to next position
@@ -41,16 +42,60 @@ namespace AdventOfCode2017.Challenges
                 skipSize++;
             }
 
-            return (list[0] * list[1]).ToString() ;
+            return (list[0] * list[1]).ToString();
         }
 
         public string Part02(string input)
         {
-            throw new NotImplementedException();
+            // populate list
+            var list = new int[ListLength];
+            for (int i = 0; i < ListLength; i++) { list[i] = i; }
+
+            int currentPosition = 0;
+            int skipSize = 0;
+
+            List<int> sequences = input.Select(e => (int)e).ToList();
+            sequences.Add(17); sequences.Add(31); sequences.Add(73); sequences.Add(47); sequences.Add(23);
+
+            for (int rounds = 0; rounds < 64; rounds++) 
+            {
+                foreach (int seq in sequences)
+                {
+                    int halfSeq = seq / 2; // 6/2=3, 5/2=2
+                    int seqenceEndIdex = currentPosition + seq - 1;
+                    for (int i = 0; i < halfSeq; i++)
+                    {
+                        int startIndex = (currentPosition + i) % ListLength;
+                        int endIndex = (seqenceEndIdex - i) % ListLength;
+
+                        // swap / reverse
+                        int swap = list[startIndex];
+                        list[startIndex] = list[endIndex];
+                        list[endIndex] = swap;
+                    }
+                    // move to next position
+                    currentPosition = (currentPosition + seq + skipSize) % ListLength;
+
+                    // increase skipsize
+                    skipSize++;
+                }
+            }
+
+            string[] output = new string[16];
+            for (int i = 0; i < 16; i++)
+            {
+                int offset = i * 16;
+                int xorValue = list[offset];
+                for (int j = 1; j < 16; j++)
+                {
+                    xorValue ^= list[(offset + j)];
+                }
+                output[i] = xorValue.ToString("X2");
+            }
+
+            return string.Join("", output).ToLower();
         }
 
         public string input = @"165,1,255,31,87,52,24,113,0,91,148,254,158,2,73,153";
-
-
     }
 }
