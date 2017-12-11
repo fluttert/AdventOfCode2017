8f6cc81eaeb7e3abf4c27702212e169b90e935a3
Ernst Fluttert <e.fluttert@gmail.com>
2017-12-11 22:03:29 +0100

    day 11 cleanup

--- AdventOfCode2017Tests/Day11Test.cs
+++ AdventOfCode2017Tests/Day11Test.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AdventOfCode2017.Challenges;
 using FluentAssertions;
-using AdventOfCode2017.Challenges;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCode2017Tests
 {
@@ -15,7 +12,8 @@ namespace AdventOfCode2017Tests
         [DataRow("ne,ne,sw,sw", "0")]
         [DataRow("ne,ne,s,s", "2")]
         [DataRow("se,sw,se,sw,sw", "3")]
-        public void Day11Part01(string input, string expectedResult) {
+        public void Day11Part01(string input, string expectedResult)
+        {
             var day = new Day11();
             day.Part01(input).Should().Be(expectedResult);
         }
