a994c44b8324cef50383fde9f58ff453ca76f510
Ernst Fluttert <e.fluttert@gmail.com>
2018-01-11 17:14:23 +0100

    Day 17 part 1

---
+++ AdventOfCode2017/Challenges/Day17.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCode2017.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2017.Challenges
+{
+    public class Day17 : IChallengeDay
+    {
+        public string Part01(string input)
+        {
+            int stepsForward = int.Parse(input);
+            int[] buffer = new int[2018];
+            buffer[1] = 1;
+            int length = 2, position = 1;
+            while (length < 2018) {
+                position = (position + stepsForward) % length;
+
+                // move all stuff further
+                for (int i = length; i > position; i--) {
+                    buffer[i] = buffer[i - 1];
+                }
+                buffer[position+1] = length;
+
+                position++;
+                length++;
+            }
+            return buffer[((position + 1) % length)].ToString();
+        }
+
+        public string Part02(string input)
+        {
+            return null;
+        }
+
+        public string input = "376";
+    }
+}
